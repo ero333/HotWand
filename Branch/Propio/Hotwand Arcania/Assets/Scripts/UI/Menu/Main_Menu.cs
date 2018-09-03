90a2ee7b84a5db80fcc797c708c7ddb1d8f5fc23
USAL <labdiseno@usal.edu.ar>
2018-09-03 19:43:10 -0300

    Agrego el branch del proyecto para refactorear el código

---
+++ Branch/Propio/Hotwand Arcania/Assets/Scripts/UI/Menu/Main_Menu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Main_Menu : MonoBehaviour {
+
+	public void PlayGame()
+	{
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+	}
+
+	public void QuitGame()
+	{
+		Application.Quit();
+	}
+}
---
+++ Branch/Propio/Hotwand Arcania/Assets/Scripts/Weapon/InteractionObject.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractionObject : MonoBehaviour {
+
+	public void DoInteraction(){
+		//Picked up and put in inventory
+		gameObject.SetActive (false);
+	}
+}
