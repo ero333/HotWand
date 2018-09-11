f652f51838514cfdc8e3cc8ba979f9d54b16562e
Fermin <fermin@fernandezdiaz.com>
2018-09-11 00:49:58 -0300

    -

---
+++ Branch/Propio/Hotwand Arcania/Assets/Scripts/Enemy/EnemyFSM.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class EnemyFSM : MonoBehaviour {
+
+	public Idle idleState;
+	public Patrol patrolState;
+	public Wander wanderState;
+	public AIDestinationSetter chaseState;
+	public WeaponPickup weaponpickupState;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
