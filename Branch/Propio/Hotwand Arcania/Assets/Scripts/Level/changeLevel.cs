b345ba33cf9d38e38de4092f3264e896c9df39f8
Fausto Castagnari <castagnari011@gmail.com>
2018-09-15 00:47:12 -0300

    Añado el changeLevel.cs al Tutorial.unity para pasar de nivel al Testing.unity.

---
+++ Branch/Propio/Hotwand Arcania/Assets/Scripts/Level/changeLevel.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class changeLevel : MonoBehaviour {
+
+    void OnTriggerEnter2D(Collider2D ChangeScene)
+    {
+        if (ChangeScene.gameObject.CompareTag("Player"))
+        {
+            SceneManager.LoadScene (1);
+            Destroy(gameObject);
+        }
+    }
+}
