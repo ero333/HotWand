4027c24ef06c44be54d69aa8cfc88cdafc436a7e
Fermin <fermin@fernandezdiaz.com>
2018-09-24 02:32:12 -0300

    Terminé el cono de visión del enemigo para con el jugador.

--- Branch/Propio/Hotwand Arcania/Assets/Scripts/Enemy/DetectPlayer.cs
+++ Branch/Propio/Hotwand Arcania/Assets/Scripts/Enemy/DetectPlayer.cs
@@ -1,41 +1,65 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+public class DetectPlayer : MonoBehaviour
+{
+	public bool playerInRange;
+	public float viewAngle;
+	public GameObject player;
+    public Transform target;
+	public Animator animator;
 
-public class DetectPlayer : MonoBehaviour {
-	// Use this for initialization
-    public bool playerInRange; // is the player within the enemy's sight range collider (this only checks if the enemy can theoretically see the player if nothing is in the way)
-	//public float detectionRange;
-    GameObject player; // a reference to the player for raycasting
-    void Start()
+	public LayerMask layersIWant;
+	 void Start()
+	 {
+		 playerInRange = false;
+	 
+	 }
+    void Update()
     {
-        playerInRange = false;
-        player = GameObject.FindGameObjectWithTag("Player");
-    }
+			Vector3 dirToTarget = target.transform.position - transform.position;
+            Vector3 C = Quaternion.AngleAxis(90 + viewAngle / 2, -transform.forward) * -transform.right;
+            if (SignedAngleBetween(transform.up, dirToTarget, transform.up) <= SignedAngleBetween(transform.up, C, transform.up) || viewAngle == 360)
+            {
+				RaycastHit2D enemySight = Physics2D.Raycast(transform.position, dirToTarget, 10.0f, layersIWant);
 
-  
-    void FixedUpdate()
-    {	
-		var direction = transform.position - player.transform.position;
-		var distanceToPlayer = Vector2.Distance(transform.position, player.transform.position);
-		RaycastHit2D enemySight = Physics2D.Linecast(transform.position, player.transform.position);
-		
-		if (enemySight.collider != null)
-		{
-			if (enemySight.collider.name == "Player")
-			{
-				playerInRange = true;
-				Debug.DrawRay(transform.position, -direction, Color.green);
+				if (enemySight.collider != null)
+				{
+					if (enemySight.collider.name == "Player")
+					{
+						playerInRange = true;
+						animator.SetBool("Player Detected", true);
+						Debug.DrawRay(transform.position, dirToTarget, Color.green);
+					}
+					else
+					{
+						playerInRange = false;
+						animator.SetBool("Player Detected", false);
+						Debug.DrawRay(transform.position, dirToTarget, Color.red);
+					}
+				}
+				else
+				{
+					playerInRange = false;
+					animator.SetBool("Player Detected", false);
+					Debug.DrawRay(transform.position, dirToTarget, Color.red);
+				}
 			}
 			else
 			{
 				playerInRange = false;
-				Debug.DrawRay(transform.position, -direction, Color.red);
+				animator.SetBool("Player Detected", false);
+				Debug.DrawRay(transform.position, dirToTarget, Color.red);
 			}
-		}
-		if (enemySight.collider != null)
-		{
-			print(enemySight.collider);
-		}
+
+    }
+
+	float SignedAngleBetween(Vector3 a, Vector3 b, Vector3 n)
+	{
+		float angle = Vector3.Angle(a, b);                                  // angle in [0,180]
+		float sign = Mathf.Sign(Vector3.Dot(n, Vector3.Cross(a, b)));       //Cross for testing -1, 0, 1
+		float signed_angle = angle * sign;                                  // angle in [-179,180]
+		float angle360 =  (signed_angle + 360) % 360;                       // angle in [0,360]
+		return (angle360);
 	}
 }
