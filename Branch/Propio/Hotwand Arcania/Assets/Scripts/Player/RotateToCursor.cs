f255fe742202df0e0bb9391e73f3dbc489bf95fe
Fausto Castagnari <castagnari011@gmail.com>
2018-09-29 12:04:29 -0300

    Arregle la rotacion del personaje para que gire siguiendo el mouse.

--- Branch/Propio/Hotwand Arcania/Assets/Scripts/Player/RotateToCursor.cs
+++ Branch/Propio/Hotwand Arcania/Assets/Scripts/Player/RotateToCursor.cs
@@ -8,7 +8,6 @@ public class RotateToCursor : MonoBehaviour {
 	
 	// Update is called once per frame
 	private void Update () {
-		if (!GetComponent<Health>().dead)
 		{
 			Vector2 direction = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
 			float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
