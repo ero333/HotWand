4419adc1b257d0babdf15ca82872676feae8983f
Fausto Castagnari <castagnari011@gmail.com>
2018-10-05 15:27:31 -0300

    Subiendo la escena para la selecion de niveles. Modificado el nombre de las escenas a lvl1; lvl2; lvl3; etc. para mas orden.

---
+++ Branch/Propio/Hotwand Arcania/Assets/Scripts/UI/Menu/LoadOnClick.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadOnClick : MonoBehaviour {
+
+	public void LoadScene(int level)
+    {
+        Application.LoadLevel(level); 
+    }
+}
