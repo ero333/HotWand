d406453ea56584ba1f40323d671d43eb0f078e22
Fermin <fermin@fernandezdiaz.com>
2018-10-05 16:40:48 -0300

    Pulí los niveles para que fueran funcionales

--- Branch/Propio/Hotwand Arcania/Assets/Scripts/Pathfinding/AstarPathfindingProject/Behaviors/Weapon.cs
+++ Branch/Propio/Hotwand Arcania/Assets/Scripts/Pathfinding/AstarPathfindingProject/Behaviors/Weapon.cs
@@ -6,7 +6,36 @@ public class Weapon : MonoBehaviour {
 	public string weaponName;
 	public bool equippable;		//If true, this object can be stored in inventory
 	//public string weaponType;		//This will tell what type of item this object is
+	public string weaponType;
+	public int weaponAmmo;
+	private bool hasAmmo;
 
+	void Start()
+	{
+		weaponType = null;
+		if (weaponName != null)
+		{
+			switch (weaponName)
+			{
+				case "Wand":
+					weaponType = "Ranged";
+				break;
+
+				case "Crossbow":
+					weaponType = "Ranged";
+				
+				break;
+
+				case "Sword":
+					weaponType = "Melee";
+				break;
+
+				case "Axe":
+					weaponType = "Melee";
+				break;
+			}
+		}
+	}
 	public void PickedUp(){
 		//Picked up and put in inventory
 		gameObject.SetActive (false);
