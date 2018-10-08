bd4e328b1a8362120b0b97ababc0cc2210a4a727
USAL <labdiseno@usal.edu.ar>
2018-10-08 20:58:05 -0300

    -

--- Branch/Propio/Hotwand Arcania/Assets/Scripts/UI/Menu/Main_Menu.cs
+++ Branch/Propio/Hotwand Arcania/Assets/Scripts/UI/Menu/Main_Menu.cs
@@ -2,11 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Analytics;
 
 public class Main_Menu : MonoBehaviour {
 
+	
+
 	public void PlayGame()
 	{
+		Analytics.CustomEvent("EmpezarNivel", new Dictionary<string,object> 
+		{
+			{"nivel", SceneManager.GetActiveScene().buildIndex + 2}
+		});
 		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
 	}
 
