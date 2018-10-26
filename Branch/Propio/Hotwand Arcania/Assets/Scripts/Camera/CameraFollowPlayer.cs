e5e862e2bb3599d4f28fbb3d68843299e046f6b6
USAL <labdiseno@usal.edu.ar>
2018-10-26 19:25:01 -0300

    Arreglé lo que pinché

--- Branch/Propio/Hotwand Arcania/Assets/Scripts/Camera/CameraFollowPlayer.cs
+++ Branch/Propio/Hotwand Arcania/Assets/Scripts/Camera/CameraFollowPlayer.cs
@@ -16,7 +16,6 @@ public class CameraFollowPlayer : MonoBehaviour {
 		Physics2D.IgnoreLayerCollision (LayerMask.NameToLayer("Enemy"), LayerMask.NameToLayer("MeleeHitboxEnemy"));
 		Physics2D.IgnoreLayerCollision (LayerMask.NameToLayer("MeleeHitboxPlayer"), LayerMask.NameToLayer("Wall"));
 		Physics2D.IgnoreLayerCollision (LayerMask.NameToLayer("MeleeHitboxEnemy"), LayerMask.NameToLayer("Wall"));
-		Physics2D.IgnoreLayerCollision (LayerMask.NameToLayer("DoorHinge"), LayerMask.NameToLayer("Wall"));
 
 	}
 	
